4f43b51162dc306e8c2b2aa14eeccad3d4726f64
Ivan Petrov <x.petrov.v.x@gmail.com>
2022-02-18 11:17:32 +0300

    V2

--- Primes.java
+++ Primes.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
+//Выводим простые числа ниже указанного числа
 public class Primes {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите число. Меньше него будут простые числа в ответе: ");
         int input = in.nextInt();
         for (int n = 2; n <= input; n++) {
-            int p = n;
             boolean outputNum = isPrime(n);
             if (outputNum)
-                System.out.print(p + " ");
+                System.out.print(n + " ");
         }
     }
-
+    //Метод, определяющий, является ли аргумент простым числом
     public static boolean isPrime(int n) {
         for (int i = 2; i < n; i++) {
             if (n % i == 0)
