9b7352d406213f0cc1cfb0f1f3de4d93d250e901
Ivan Petrov <x.petrov.v.x@gmail.com>
2022-05-20 09:43:06 +0300

    V1

---
+++ src/com/company/Location.java
@@ -0,0 +1,56 @@
+package com.company;
+
+/**
+ * This class represents a specific location in a 2D map.  Coordinates are
+ * integer values.
+ **/
+public class Location {
+    /**
+     * X coordinate of this location.
+     **/
+    public int xCoord;
+
+    /**
+     * Y coordinate of this location.
+     **/
+    public int yCoord;
+
+
+    /**
+     * Creates a new location with the specified integer coordinates.
+     **/
+    public Location(int x, int y) {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    /**
+     * Creates a new location with coordinates (0, 0).
+     **/
+    public Location() {
+        this(0, 0);
+    }
+
+    /**
+     * Compares this Location to another.
+     **/
+    public boolean equals(Object obj) {
+        if (obj instanceof Location) {
+            Location other = (Location) obj;
+            if (xCoord == other.xCoord && yCoord == other.yCoord) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Provides a hashCode for each Location.
+     **/
+    public int hashCode() {
+        int result = 17;
+        result = 37 * result + xCoord;
+        result = 37 * result + yCoord;
+        return result;
+    }
+}
